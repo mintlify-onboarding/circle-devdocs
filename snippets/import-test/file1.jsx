72161e5502ffa73065592e0f49f1ebd4c271207d
gabrielraeder <gabrielraeder@outlook.com.br>
2025-10-21 19:29:38 -0300

    save test files

---
+++ snippets/import-test/file1.jsx
@@ -0,0 +1,8 @@
+export const ChildComponent = () => {
+  return (
+    <div className="p-2.5 border border-blue-500 rounded">
+      <h3 className="font-bold text-blue-600">This is the Child Component</h3>
+      <p className="text-gray-700">I'm a simple child component nested inside the parent.</p>
+    </div>
+  );
+};
